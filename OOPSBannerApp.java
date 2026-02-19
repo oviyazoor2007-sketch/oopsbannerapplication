46969baf64be92507d3baaf6e3ba7acd3a482755
oviyazoor2007 <oviyazoor2007@gmail.com>
2026-02-19 15:39:27 +0530

    UC5 - Implemented inline array initialization using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,16 +2,15 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Create String array to store banner lines
-        String[] lines = new String[7];
-
-        lines[0] = String.join(" ", "  ***  ", "  ***  ", " ***** ", " ***** ");
-        lines[1] = String.join(" ", " **  ** ", " **  ** ", " **  ** ", " **     ");
-        lines[2] = String.join(" ", " **  ** ", " **  ** ", " ****   ", "  ***   ");
-        lines[3] = String.join(" ", " **  ** ", " **  ** ", " **     ", "     ** ");
-        lines[4] = String.join(" ", " **  ** ", " **  ** ", " **     ", "     ** ");
-        lines[5] = String.join(" ", "  ***   ", "  ***   ", " **     ", " ****   ");
-        lines[6] = String.join(" ", "        ", "        ", "        ", "        ");
+        // Inline array initialization
+        String[] lines = {
+            String.join(" ", "  ***  ", "  ***  ", "   *** ", "    ***** "),
+            String.join(" ", " **  ** ", " **  ** ", " **  ** ", " **     "),
+            String.join(" ", " **  ** ", " **  ** ", " ****   ", "  ***   "),
+            String.join(" ", " **  ** ", " **  ** ", " **     ", "     ** "),
+            String.join(" ", " **  ** ", " **  ** ", " **     ", "     ** "),
+            String.join(" ", "  ***   ", "  ***   ", " **     ", " ****   ")
+        };
 
         // Print using for-each loop
         for (String line : lines) {
